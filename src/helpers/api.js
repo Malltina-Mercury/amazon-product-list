a04b907a05d4dee2d307f9a9476c578d8d5f2185
Hamidreza <hamidlashgari1998@gmail.com>
2022-05-09 17:06:25 +0430

    Create api request helpers

---
+++ src/helpers/api.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const BASE_URL = 'https://api.malltina.net/search/v2?q='
+
+
+const request = axios.create({
+    baseURL: BASE_URL,
+    timeout: 10000,
+    headers: { 'Content-Type': 'application/json' }
+  })
+  
