bf8d15921531cc3624531bfdeeb695e611809fde
Masud Afsar <masud@afsar.info>
2022-05-10 16:11:57 +0430

    Add input box

--- src/components/SearchBtn.js
+++ src/components/SearchBtn.js
@@ -1,8 +1,28 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {StyleSheet, TextInput} from 'react-native';
 
 const SearchBtn = () => {
-  return <Text>SearchBtn</Text>;
+  const [number, onChangeNumber] = React.useState(null);
+
+  return (
+    <>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeNumber}
+        value={number}
+        placeholder="useless placeholder"
+        keyboardType="web-search"
+      />
+    </>
+  );
 };
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
 
 export default SearchBtn;
