e70e271933b02e21008f5d5eb5fdb1a62e50fe96
Hamidreza <hamidlashgari1998@gmail.com>
2022-05-10 19:07:37 +0430

    recycler view function

--- src/views/RecycleList.js
+++ src/views/RecycleList.js
@@ -1,31 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {Text, View} from 'react-native';
+import {RecyclerListView, DataProvider, layoutProvider} from 'recyclerlistview';
 import SearchBtn from '../components/SearchBtn';
 import useSearchApi from '../hooks/useSearchApi';
 
-const RecycleList = () => {
-  const {data, isLoaded} = useSearchApi('sony', []);
+// const RecycleList = () => {
+//   const {data, isLoaded} = useSearchApi('apple', []);
 
-  return (
-    <View>
-      {isLoaded ? (
-        <>
-          <SearchBtn />
-          <>
-            {data?.products?.map(item => (
-              <Text>
-                {item.title}
-                {'\n'}
-                ------------
-              </Text>
-            ))}
-          </>
-        </>
-      ) : (
-        <Text>Loading...</Text>
-      )}
-    </View>
-  );
-};
+//   let { width } = Dimensions.get('window');
 
-export default RecycleList;
+
+//   const dataDataProvider = new DataProvider((r1, r2) => {
+//     return r1 !== r2;
+//   }).cloneWithRows(data);
+
+//   const layoutsProvider = new layoutProvider(
+//     () => 0,
+//     (type, dim) => {
+//       dim.width = width;
+//       dim.height = 240;
+//     },
+
+
+//   return (
+//     <>
+//     <View>
+//       <RecyclerListView
+//         layoutProvider={layoutsProvider}
+//         dataProvider={dataDataProvider}
+//       />
+//     </View>
+//     </>
+//   );
+// };
+
+// export default RecycleList;
+
+
+
+export default class RecycleList extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      dataProvider: new DataProvider((r1, r2) => {
+          return r1 !== r2
+      }).cloneWithRows(props)
+  };
+  }
+
+
+
+
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
